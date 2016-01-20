4be68a1cc89a5fd6f8465e649f63b737f37a6112
Nate Brune <natebrune@gmail.com>
2016-01-19 20:31:53 -0500

    kfree

--- src/kernel.c
+++ src/kernel.c
@@ -12,6 +12,7 @@ int kmain(struct multiboot *mboot_ptr)
     monitor_write("Initializing PIT... ", white); init_timer(100); monitor_write("ok\n", green);
     monitor_write("Initializing paging... ", white); initialise_paging(100); monitor_write("ok\n", green);
     u32int *ptr = (u32int*)0xA0000000;
-    u32int do_page_fault = *ptr;
+    u32int *alsoptr = ptr;
+    u32int do_page_fault = *alsoptr;
     return 0;
 }
