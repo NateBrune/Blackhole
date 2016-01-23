277a897070fbc662591b28545cd66b083cc88c04
Nate Brune <natebrune@gmail.com>
2016-01-23 11:10:59 -0500

    renamed

--- src/kernel.c
+++ src/kernel.c
@@ -8,7 +8,7 @@ int kmain(struct multiboot *mboot_ptr)
 {
     monitor_clear();
     monitor_write("\n\n", white);
-    monitor_write_center("Welcome to ImperitOS\n\n\n", white);
+    monitor_write_center("Welcome to the blackhole\n\n\n", white);
     monitor_write("Initializing descriptors... ", white); init_descriptor_tables(); monitor_write("ok\n", green);
     monitor_write("Initializing PIT... ", white); init_timer(100); monitor_write("ok\n", green);
     monitor_write("Initializing paging... ", white); init_paging(); monitor_write("ok\n", green);
